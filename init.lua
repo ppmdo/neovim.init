96a629d2692a8d36b8c6ad9f82eb00f6ae17ca3b
Pablo Prieto <pabloprieto@live.com>
2023-12-04 23:34:28 +0100

    switch to lazy, disabled harpoon

--- init.lua
+++ init.lua
@@ -1,1 +1,33 @@
-require("devpabs")
+require('devpabs')
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {'nvim-lua/plenary.nvim'},
+    {'nvim-telescope/telescope.nvim', tag = '0.1.5'},
+    {'rose-pine/neovim'},
+    {'nvim-treesitter/nvim-treesitter'},
+    {'mbbill/undotree'},
+    {'tpope/vim-fugitive'},
+
+    --- Uncomment these if you want to manage LSP servers from neovim
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+})
